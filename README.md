89aab57638622d68e8bbbd07c137bd8fba18589b
Ali Shah <s.ashah@outlook.com>
2021-05-17 12:02:05 -0400

    [Update] decided to make ATLA quote bot

--- README.md
+++ README.md
@@ -3,18 +3,20 @@
 ![npm (scoped)](https://img.shields.io/npm/v/@shah-a/node-module-challenge?style=for-the-badge)
 ![npm](https://img.shields.io/npm/dt/@shah-a/node-module-challenge?style=for-the-badge)
 
-## Description
+## ATLA Quote Bot
 
-Not sure what this will do just yet... but it'll do something :)
+### Description
 
-## Installation
+A simple node module that returns random quotes from various `Avatar: The Last Airbender` scenes.
+
+### Installation
 
 Local installation to any node project:
 
 ```
-npm install @shah-a/node-module-challenge --save
+npm install @shah-a/atla --save
 ```
 
-## Usage
+### Usage
 
 How to use...
--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-const saySalaam = () => {
-  console.log("Salaam world! :D I'm publishing a node module!!");
-};
+// Response set 1 for Aang
+const aang = [
+  "Aang: \"Harsh words won't solve problems, action will!\"", // S1E11
+  "Aang: \"The past can be a great teacher.\"", // S3E13
+  "Aang: \"It's easy to do nothing, but it's hard to forgive.\"", // S3E16
+]
+
+// Response set 2 for Katara
+const katara = [
+  "Katara: \"It is the strength of your hearts that make you who you are.\"", // S1E6
+  "Katara: \"Everybody, hold hands. We can do this. We have to.\"", // S2E11
+  "Katara: \"I will never, ever turn my back on people who need me!\"", // S3E3
+]
+
+// Response set 3 for Sokka
+const sokka = [
+  "Sokka: \"I'm just a guy with a boomerang. I didn't ask for all this flying and magic!\"", // S1E2
+  "Sokka: \"It's a giant mushroom! Maybe it's friendly!\"", // S2E11
+  "Sokka: \"I'm just a guy who likes comedy.\"", // S3E17
+]
+
+// Response set 4 for Uncle Iroh
+const iroh = [
+  "Uncle Iroh: \"Pride is not the opposite of shame, but its source. True humility is the only antidote to shame.\"", // S2E9
+  "Uncle Iroh: \"It is usually best to admit mistakes when they occur, and seek to restore honor.\"", // S2E15
+  "Uncle Iroh: \"You can't always see the light at the end of the tunnel, but if you just keep moving, you will come to a better place.\"" // S2E20
+]
+
+// Response set 5 for Cabbage Merchant
+const cabbage = [
+  "Cabbage Merchant: \"No! My cabbages!\"", // S1E5
+  "Cabbage Merchant: \"My cabbages! This place is worse than Omashu!\"", // S1E9
+  "Cabbage Merchant: \"My cabb--!... Oh, forget it!\"" // S2E15
+]
+
+const getQuote = () => {
+  console.log(cabbage[1])
+}
 
 module.exports = {
-  saySalaam
+  getQuote
 };
